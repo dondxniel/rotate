5bea797e81134293993de7d5d09b741db02d94f4
Don Daniel <dondaniel028@gmail.com>
2025-01-13 03:10:14 +0100

    Hydration error fixed.

--- components/cards/suggestion-card.tsx
+++ components/cards/suggestion-card.tsx
@@ -1,12 +1,21 @@
+'use client';
 import Badge from '@/components/ui/badge';
 import { HStack, Text, VStack } from '@chakra-ui/react';
 import { CaretUp, ChatCircle } from '@phosphor-icons/react';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function SuggestionCard() {
-	const { theme } = useTheme();
+	const { resolvedTheme } = useTheme();
+	const [theme, setTheme] = useState<string | undefined | null>(null);
+
+	useEffect(() => {
+		setTheme(resolvedTheme); // Update theme after hydration
+	}, [resolvedTheme]);
+
 	return (
 		<HStack
+			suppressHydrationWarning
 			data-state='open'
 			_open={{
 				animation: 'fade-in 300ms ease-out',
@@ -16,20 +25,29 @@ export default function SuggestionCard() {
 			p='32px'
 			gapX={'40px'}
 			borderRadius={'10px'}
-			bg={{ base: '#fff', _dark: '#333' }}
+			bg={{
+				base: '#fff',
+				_dark: '#333',
+			}}
 			className='group cursor-pointer'
 		>
 			<VStack className='!hidden md:!flex'>
 				<VStack
 					width={'40px'}
-					bg={{ base: '#F2F4FE', _dark: '#4661E6' }}
+					bg={{
+						base: '#f2f4fe',
+						_dark: '#4661e6',
+					}}
 					borderRadius={'10px'}
 					p='8px'
 					fontSize='sm'
 					fontWeight={'bold'}
 					_hover={{ background: '#CFD7FF' }}
-					color={{ _dark: 'white' }}
-					className={`transition-all duration-300 cursor-pointer`}
+					color={{
+						base: 'inherit',
+						_dark: 'white',
+					}}
+					className='transition-all duration-300 cursor-pointer'
 				>
 					<CaretUp
 						weight='bold'
@@ -41,7 +59,7 @@ export default function SuggestionCard() {
 			<VStack flex={1} alignItems={'start'} gap={0}>
 				<Text
 					color={{
-						base: '#3A4374',
+						base: '#3a4374',
 						_dark: '#fff',
 					}}
 					className={`font-bold text-lg ${
@@ -53,8 +71,13 @@ export default function SuggestionCard() {
 				>
 					Add tags for solutions
 				</Text>
-				<Text color={{ base: '#647196', _dark: '#a6b0cd' }}>
-					Easier to search for solutions based on a specific stack.{' '}
+				<Text
+					color={{
+						base: '#647196',
+						_dark: '#a6b0cd',
+					}}
+				>
+					Easier to search for solutions based on a specific stack.
 				</Text>
 				<HStack flexWrap={'wrap'} gapX={'8px'} mt='5'>
 					<Badge>Enhancement</Badge>
@@ -65,14 +88,20 @@ export default function SuggestionCard() {
 					className='!flex md:!hidden w-full'
 				>
 					<HStack
-						bg={{ base: '#F2F4FE', _dark: '#4661E6' }}
+						bg={{
+							base: '#f2f4fe',
+							_dark: '#4661e6',
+						}}
 						borderRadius={'10px'}
 						p='8px'
 						fontSize='sm'
 						fontWeight={'bold'}
 						_hover={{ background: '#CFD7FF' }}
-						color={{ _dark: 'white' }}
-						className={`transition-all duration-300 cursor-pointer`}
+						color={{
+							base: 'inherit',
+							_dark: 'white',
+						}}
+						className='transition-all duration-300 cursor-pointer'
 					>
 						<CaretUp
 							weight='bold'
@@ -80,13 +109,23 @@ export default function SuggestionCard() {
 						/>
 						112
 					</HStack>
-					<HStack color={{ _dark: '#fff' }}>
+					<HStack
+						color={{
+							base: 'inherit',
+							_dark: '#fff',
+						}}
+					>
 						<ChatCircle weight='fill' color='#CDD2EE' /> 2
 					</HStack>
 				</HStack>
 			</VStack>
 			<VStack justifyContent={'center'} className='!hidden md:!flex'>
-				<HStack color={{ _dark: '#fff' }}>
+				<HStack
+					color={{
+						base: 'inherit',
+						_dark: '#fff',
+					}}
+				>
 					<ChatCircle weight='fill' color='#CDD2EE' /> 2
 				</HStack>
 			</VStack>
